03b9205a035387ab9d7318d1fd05a65e43ca91a2
Maria Alexandra Yukyi Chang Fonseca <59944424+AlexandraYuyi@users.noreply.github.com>
2024-06-06 10:06:31 -0400

    fix: recognize multiline comments and ignore them

--- lexer.py
+++ lexer.py
@@ -26,7 +26,7 @@ class Lexer:
             kind = match.lastgroup
             value = match.group(kind)
 
-            if kind == 'COMMENT' or kind == 'WHITESPACE' or kind == 'NEWLINE':
+            if kind == 'ONELINE_COMMENT' or kind == 'MULTILINE_COMMENT' or kind == 'WHITESPACE' or kind == 'NEWLINE':
                 # Ignore the full line of code that contains the comment symbols
                 continue
             elif kind == 'MISMATCH':
